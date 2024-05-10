b34df2c43b449d13083a47f5e0a21b94a57aa13e
hasetz <hjmsetz@gmail.com>
2024-05-11 00:33:06 +0200

    Added compression support for pdfs

--- monitor.py
+++ monitor.py
@@ -1,11 +1,14 @@
 import logging
-import sys
+import shutil
+import subprocess
+import tempfile
 import time
 import yaml
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler, PatternMatchingEventHandler
 from PyPDF2 import PdfWriter, PdfReader
 from pathlib import Path
+
 
 config = yaml.safe_load(open("config.yaml"))
 
@@ -20,38 +23,80 @@ class pdf_handler(PatternMatchingEventHandler):
 
     def on_created(self, event):
         """Triggered when pdf file is created in the workload folder. Preprocesses the pdf file and saves it in the preprocess folder."""
-        reducted_pdf = preprocess_pdf(event.src_path)
         filename = event.src_path.split("/")[-1]
-        filepath = Path(self.config["preprocess_folder"]) / ('preprocessed_' + filename)
-        with open(filepath, 'wb') as f:
-            reducted_pdf.write(f)  
+        output_path = Path(self.config["preprocess_folder"]) / filename
+        preprocess_pdf(event.src_path, output_path)
 
-def preprocess_pdf(file_path):
+def get_ghostscript_path():
+    gs_names = ["gs", "gswin32", "gswin64"]
+    for name in gs_names:
+        if shutil.which(name):
+            return shutil.which(name)
+    raise FileNotFoundError(
+        f"No GhostScript executable was found on path ({'/'.join(gs_names)})"
+    )
+
+def compress_pdf_file(input_path, output_path):
+    """Compresses the pdf file using ghostscript."""
+    gs = get_ghostscript_path()
+    tmp = tempfile.TemporaryDirectory()
+    logging.info(f"Started compressing {input_path} to {output_path}")
+    subprocess.call(
+        [
+            gs,
+            "-q"
+            "-dNOPAUSE",
+            "-dBATCH",
+            "-dSAFER",
+            "-dQUIET",
+            "-sDEVICE=pdfwrite",
+            "-dCompatibilityLevel=1.5",
+            "-dPDFSETTINGS=/screen",
+            "-dEmbedAllFonts=true", 
+            "-dSubsetFonts=true",  
+            "-dColorImageDownsampleType=/Bicubic", 
+            "-dColorImageResolution=144",                #PDF downsample color image resolution`
+            "-dGrayImageDownsampleType=/Bicubic", 
+            "-dGrayImageResolution=144",                 #PDF downsample gray image resolution`
+            "-dMonoImageDownsampleType=/Bicubic",
+            "-dMonoImageResolution=144",                  
+            "-sOutputFile=" + tmp.name + "/output.pdf",
+            "-dNOPAUSE",
+            str(input_path),
+        ]
+    )
+    shutil.move(tmp.name + "/output.pdf", output_path)
+    tmp.cleanup()
+    logging.info(f"Compressed {input_path} to {output_path}")
+
+def preprocess_pdf(input_path, output_path):
     """Preprocesses the pdf file by removing the first and last page."""
-    reader = PdfReader(file_path)
+    reader = PdfReader(input_path)
     writer = PdfWriter()
     pages_to_keep = reader.pages[1:-1] # Delete first and last page
     for page in pages_to_keep:
         writer.add_page(page)
-    return writer
+    with open(output_path, 'wb') as f:
+        writer.write(f) 
+    compress_pdf_file(output_path, Path(config['final_folder']) / output_path.name)
+    
 
 def check_new_pdfs(workload_folder, preprocess_folder):
     """When application is started, checks for any pdf files in the workload folder and preprocesses them."""
     for file in Path(workload_folder).glob('*.pdf'):
-        if not (Path(preprocess_folder) / ('preprocessed_' + file.name)).exists():
-            reducted_pdf = preprocess_pdf(file)
-            filepath = Path(preprocess_folder) / ('preprocessed_' + file.name)
-            with open(filepath, 'wb') as f:
-                reducted_pdf.write(f)
+        if not (Path(preprocess_folder) /  file.name).exists():
+            output_path = Path(preprocess_folder) / file.name
+            preprocess_pdf(file, output_path)
 
 
 if __name__ == "__main__":
     logging.basicConfig(filename='monitor.log', level=logging.INFO)
+    logging.getLogger().addHandler(logging.StreamHandler())
     workload_folder = config["workload_folder"]
-    preprocess_folder = config["preprocess_folder"]
-    Path(config["preprocess_folder"]).mkdir(parents=True, exist_ok=True)
+    for folder in [config["preprocess_folder"], config['final_folder']]:
+        Path(folder).mkdir(parents=True, exist_ok=True)
 
-    check_new_pdfs(workload_folder, preprocess_folder)
+    check_new_pdfs(workload_folder, config["preprocess_folder"])
 
     # Handler for logging all events in the workload folder
     logging_handler = LoggingEventHandler()
